a89f1aade26140408598af0e4c83810a225a8324
Remi <rmikel@clever-age.com>
2022-11-07 18:12:51 +0900

    Homepage and translation

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/{_locale}', name: 'app_homepage')]
+    public function index(TranslatorInterface $translator, Request $request): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+}
