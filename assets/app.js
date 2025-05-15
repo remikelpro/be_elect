e15a4753bd9715b81ff261fc4e458f41ee49709e
Clem-Coding <clementine.blondeau@gmail.com>
2025-05-15 17:01:47 +0200

    integration of the elections page

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,9 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
-import 'bootstrap';
-import 'bootstrap-autocomplete';
-import 'select2';
-import './main';
+import "./styles/app.scss";
+import "bootstrap";
+import "bootstrap-autocomplete";
+import "select2";
+import "./main";
+import "./search-election";
