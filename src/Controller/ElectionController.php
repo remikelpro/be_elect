50a8107d1084abd9bb7b9747da94e3d07a037189
Clem-Coding <clementine.blondeau@gmail.com>
2025-05-16 17:44:00 +0200

    adjust font sizes and add breadcrumb

--- src/Controller/ElectionController.php
+++ src/Controller/ElectionController.php
@@ -6,19 +6,25 @@ use App\Repository\TypeElectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
-class ElectionController extends AbstractController
+class ElectionController extends AbstractBeElectController
 {
-    public function __construct(private TypeElectionRepository $typeElectionRepository)
-    {
-        
-    }
-    
+    public function __construct(
+        private TypeElectionRepository $typeElectionRepository,
+        private TranslatorInterface $translator
+    ) {}
+
     #[Route('/elections', name: 'elections')]
     public function index(): Response
     {
+
+        $breadcrumb = $this->getBreadcrumb([
+            ['name' => $this->translator->trans('Elections'), 'href' => $this->generateUrl('elections')]
+        ]);
+
         return $this->render('election/index.html.twig', [
-            'controller_name' => 'ElectionController',
+            'breadcrumb' => $breadcrumb,
         ]);
     }
 
